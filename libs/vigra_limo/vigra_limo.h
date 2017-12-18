1b252bf268feb52573186cd47bd69c173d205bd8
Moritz Molle <mmolle@gmx.net>
2017-12-18 01:48:52 +0100

    forgot to add test.limo and vigra_limo.h

---
+++ libs/vigra_limo/vigra_limo.h
@@ -0,0 +1,6 @@
+// include after limo.h!
+extern limo_data *cvector_type_sym;
+BUILTIN(builtin_vigra_structuretensor_c);
+BUILTIN(builtin_vigra_tensoreigenrepresentation_c);
+BUILTIN(builtin_vigra_hessianmatrixofgaussian_c);
+BUILTIN(builtin_vigra_upwindimage_c);
