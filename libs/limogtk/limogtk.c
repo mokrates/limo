62f3f57ad0e2ee2c0af629176e00afca5dc04d42
Moritz Molle <moritz.molle@netlution.de>
2020-06-19 02:18:24 +0200

    das ist jetzt der limogtk-stand fuer heute

--- libs/limogtk/limogtk.c
+++ libs/limogtk/limogtk.c
@@ -45,20 +45,20 @@ BUILTIN(builtin_gtk_init_check)
 /*   return nil; */
 /* } */
 
-BUILTIN(builtin_gtk_window_set_default_size)
-{
-  limo_data *ld_window, *ld_width, *ld_height;
-  GtkWidget *window;
-  REQUIRE_ARGC("GTK-WINDOW-SET-DEFAULT-SIZE", 3);
-  ld_window = eval(FIRST_ARG, env);
-  ld_width = eval(SECOND_ARG, env);
-  ld_height = eval(THIRD_ARG, env);
-  window = get_special(ld_window, sym_limogtk_gpointer);
-  REQUIRE_TYPE("GTK-WINDOW-SET-DEFAULT-SIZE", ld_width, limo_TYPE_GMPQ);
-  REQUIRE_TYPE("GTK-WINDOW-SET-DEFAULT-SIZE", ld_height, limo_TYPE_GMPQ);
-  gtk_window_set_default_size(GTK_WINDOW(window), GETINTFROMMPQ(ld_width), GETINTFROMMPQ(ld_height));
-  return nil;
-}
+/* BUILTIN(builtin_gtk_window_set_default_size) */
+/* { */
+/*   limo_data *ld_window, *ld_width, *ld_height; */
+/*   GtkWidget *window; */
+/*   REQUIRE_ARGC("GTK-WINDOW-SET-DEFAULT-SIZE", 3); */
+/*   ld_window = eval(FIRST_ARG, env); */
+/*   ld_width = eval(SECOND_ARG, env); */
+/*   ld_height = eval(THIRD_ARG, env); */
+/*   window = get_special(ld_window, sym_limogtk_gpointer); */
+/*   REQUIRE_TYPE("GTK-WINDOW-SET-DEFAULT-SIZE", ld_width, limo_TYPE_GMPQ); */
+/*   REQUIRE_TYPE("GTK-WINDOW-SET-DEFAULT-SIZE", ld_height, limo_TYPE_GMPQ); */
+/*   gtk_window_set_default_size(GTK_WINDOW(window), GETINTFROMMPQ(ld_width), GETINTFROMMPQ(ld_height)); */
+/*   return nil; */
+/* } */
 
 /* BUILTIN(builtin_gtk_widget_show_all) */
 /* { */
@@ -105,29 +105,29 @@ BUILTIN(builtin_g_signal_connect)
                                                      (gpointer)make_cons(ld_handler, env)));
 }
 
-BUILTIN(builtin_gtk_application_new)
-{
-  limo_data *ld_app_id, *ld_app_flags, *ld_result;
-  GtkApplication *app;
-  char *app_id;
-  GApplicationFlags flags;
+/* BUILTIN(builtin_gtk_application_new) */
+/* { */
+/*   limo_data *ld_app_id, *ld_app_flags, *ld_result; */
+/*   GtkApplication *app; */
+/*   char *app_id; */
+/*   GApplicationFlags flags; */
 
-  REQUIRE_ARGC("GTK-APPLICATION-NEW", 2);
-  ld_app_id = eval(FIRST_ARG, env);
-  ld_app_flags = eval(SECOND_ARG, env);
-  if (is_nil(ld_app_id))
-    app_id = NULL;
-  else {
-    REQUIRE_TYPE("GTK-APPLICATION-NEW", ld_app_id, limo_TYPE_STRING);
-    app_id = ld_app_id->data.d_string;
-  }
-  REQUIRE_TYPE("GTK-APPLICATION-NEW", ld_app_flags, limo_TYPE_GMPQ);
-  flags  = GETINTFROMMPQ(ld_app_flags);
-  app = gtk_application_new(app_id, flags);
-  ld_result =  make_special(sym_limogtk_gpointer, app);
-  GC_register_finalizer(ld_result, (GC_finalization_proc)limogtk_finalizer_g_object_unref, app, NULL, NULL);
-  return ld_result;
-}
+/*   REQUIRE_ARGC("GTK-APPLICATION-NEW", 2); */
+/*   ld_app_id = eval(FIRST_ARG, env); */
+/*   ld_app_flags = eval(SECOND_ARG, env); */
+/*   if (is_nil(ld_app_id)) */
+/*     app_id = NULL; */
+/*   else { */
+/*     REQUIRE_TYPE("GTK-APPLICATION-NEW", ld_app_id, limo_TYPE_STRING); */
+/*     app_id = ld_app_id->data.d_string; */
+/*   } */
+/*   REQUIRE_TYPE("GTK-APPLICATION-NEW", ld_app_flags, limo_TYPE_GMPQ); */
+/*   flags  = GETINTFROMMPQ(ld_app_flags); */
+/*   app = gtk_application_new(app_id, flags); */
+/*   ld_result =  make_special(sym_limogtk_gpointer, app); */
+/*   GC_register_finalizer(ld_result, (GC_finalization_proc)limogtk_finalizer_g_object_unref, app, NULL, NULL); */
+/*   return ld_result; */
+/* } */
 
 #include "_gtk-functions.h"
 
@@ -141,10 +141,10 @@ void limo_init_limogtk(limo_data *env)
   INS_LIMOGTK_BUILTIN(builtin_gtk_init_check, "GTK-INIT-CHECK");
   INS_LIMOGTK_BUILTIN(builtin_g_application_run, "G-APPLICATION-RUN");
   INS_LIMOGTK_BUILTIN(builtin_g_signal_connect, "G-SIGNAL-CONNECT");
-  INS_LIMOGTK_BUILTIN(builtin_gtk_application_new, "GTK-APPLICATION-NEW");
+  /* INS_LIMOGTK_BUILTIN(builtin_gtk_application_new, "GTK-APPLICATION-NEW"); */
   /* INS_LIMOGTK_BUILTIN(builtin_gtk_application_window_new, "GTK-APPLICATION-WINDOW-NEW"); */
   /* INS_LIMOGTK_BUILTIN(builtin_gtk_widget_show_all, "GTK-WIDGET-SHOW-ALL"); */
-  INS_LIMOGTK_BUILTIN(builtin_gtk_window_set_default_size, "GTK-WINDOW-SET-DEFAULT-SIZE");
+  /* INS_LIMOGTK_BUILTIN(builtin_gtk_window_set_default_size, "GTK-WINDOW-SET-DEFAULT-SIZE"); */
   /* INS_LIMOGTK_BUILTIN(builtin_gtk_window_set_title, "GTK-WINDOW-SET-TITLE"); */
 
   #include "_gtk-inserts.h"
