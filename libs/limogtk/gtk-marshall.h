8aebde2ff10546be1aac91ffbcd5ad38d089cd8a
Moritz Molle <moritz.molle@netlution.de>
2020-06-30 15:37:44 +0200

    limogtk updated

---
+++ libs/limogtk/gtk-marshall.h
@@ -0,0 +1,124 @@
+typedef struct LIMO_MARSHAL_DATA {
+  GType expected_return_type;
+} limo_marshal_data;
+
+void limo_closure_marshal(GClosure *closure, GValue *return_value, guint n_param_values, const GValue *param_values, gpointer invocation_hint, gpointer marshal_data)
+{
+  int i;
+  limo_data *arglist, **next_arg, *arg, **darg, *result;
+  limo_marshal_data *lmd = (limo_marshal_data *)marshal_data;
+  limo_data *lcd = closure->data;  // limo closure data
+
+  next_arg = &arglist;
+  for (i=0; i<n_param_values; ++i){ 
+   switch (G_VALUE_TYPE(&param_values[i])) {
+    case G_TYPE_CHAR:
+      arg = make_number_from_long_long(g_value_get_char(&param_values[i]));
+      break;
+
+    case G_TYPE_UCHAR:
+      arg = make_number_from_long_long(g_value_get_uchar(&param_values[i]));
+      break;
+
+    case G_TYPE_BOOLEAN:
+      arg = make_number_from_long_long(g_value_get_boolean(&param_values[i]));
+      break;
+
+    case G_TYPE_INT:
+      arg = make_number_from_long_long(g_value_get_int(&param_values[i]));
+      break;
+
+    case G_TYPE_UINT:
+      arg = make_number_from_long_long(g_value_get_uint(&param_values[i]));
+      break;
+
+    case G_TYPE_LONG:
+      arg = make_number_from_long_long(g_value_get_long(&param_values[i]));
+      break;
+
+    case G_TYPE_ULONG:
+      arg = make_number_from_long_long(g_value_get_ulong(&param_values[i]));
+      break;
+
+    case G_TYPE_INT64:
+      arg = make_number_from_long_long(g_value_get_int64(&param_values[i]));
+      break;
+
+    case G_TYPE_UINT64:
+      arg = make_number_from_long_long(g_value_get_uint64(&param_values[i]));
+      break;
+
+    case G_TYPE_ENUM:
+      arg = make_number_from_long_long(g_value_get_enum(&param_values[i]));
+      break;
+
+    case G_TYPE_FLAGS:
+      arg = make_number_from_long_long(g_value_get_flags(&param_values[i]));
+      break;
+
+    case G_TYPE_FLOAT:
+    case G_TYPE_DOUBLE:
+      limo_error("marshaling floats is unimplemented yet");
+      break;
+
+    case G_TYPE_STRING:
+      arg = make_string(g_value_get_string(&param_values[i]));
+      break;
+
+    case G_TYPE_POINTER:
+      arg = make_special(sym_limogtk_gpointer, g_value_get_pointer(&param_values[i]));
+      break;
+
+    default:
+      if (g_value_type_compatible(G_VALUE_TYPE(&param_values[i]), G_TYPE_OBJECT))
+        arg = make_special(sym_limogtk_gpointer, g_value_get_object(&param_values[i]));
+      else
+        limo_error("tried to marshal unknown datatype: %s in %ith argument", G_VALUE_TYPE_NAME(&param_values[i]), i);
+    }
+
+    (*next_arg) = make_cons(arg, nil);
+    next_arg = &CDR(*next_arg);
+  }
+
+  result = eval(make_cons(CAR(lcd), arglist), CDR(lcd));
+
+  /* if (return_value) */
+  /*   printf("marshal: a return_value seems to be excepted... - return_value has type %s\n", G_VALUE_TYPE_NAME(return_value)); */
+  //  return_value = irgendwie_casten(result, lmd->expected_return_type);
+}
+
+GClosure *limogtk_closure_new(limo_data *ld_closure)
+{
+  GClosure *lcl;
+
+  lcl = (GClosure *)g_closure_new_simple(sizeof (GClosure), ld_closure);
+  g_closure_set_marshal(lcl, limo_closure_marshal);
+  return lcl;
+}
+
+BUILTIN(builtin_g_signal_connect)
+{
+  limo_data *ld_instance, *ld_signal, *ld_handler, *ld_after, *ld_closure;
+  GClosure *closure;
+
+  REQUIRE_ARGC("G-SIGNAL-CONNECT", 4);
+  ld_instance = eval(FIRST_ARG, env);
+  ld_signal   = eval(SECOND_ARG, env);
+  ld_handler  = eval(THIRD_ARG, env);
+  ld_after    = eval(FOURTH_ARG, env);
+
+  REQUIRE_TYPE("G-SIGNAL-CONNECT", ld_signal, limo_TYPE_STRING);
+  REQUIRE_TYPE("G-SIGNAL-CONNECT", ld_handler, limo_TYPE_LAMBDA);
+  REQUIRE_TYPE("G-SIGNAL-CONNECT", ld_instance, limo_TYPE_SPECIAL);
+
+  ld_closure = make_cons(ld_handler, env);  
+  closure = limogtk_closure_new(ld_closure);
+    
+  if (make_number_from_long_long(g_signal_connect_closure(G_OBJECT(CDR(ld_instance->data.d_special) -> data.d_special_intern),
+                                                          ld_signal->data.d_string,
+                                                          closure,
+                                                          !is_nil(ld_after))))
+    return ld_closure;
+  else
+    return nil;
+}
--- libs/limogtk/limogtk.c
+++ libs/limogtk/limogtk.c
@@ -21,56 +21,6 @@ BUILTIN(builtin_gtk_init_check)
   return make_number_from_long_long(gtk_init_check(NULL, NULL));
 }
 
-
-/* BUILTIN(builtin_gtk_application_window_new) */
-/* { */
-/*   limo_data *ld_app; */
-/*   gpointer app; */
-/*   REQUIRE_ARGC("GTK-APPLICATION-WINDOW-NEW", 1); */
-/*   ld_app = eval(FIRST_ARG, env); */
-/*   app = get_special(ld_app, sym_limogtk_gpointer); */
-/*   return make_special(sym_limogtk_gpointer, gtk_application_window_new(GTK_APPLICATION(app))); */
-/* } */
-
-/* BUILTIN(builtin_gtk_window_set_title) */
-/* { */
-/*   limo_data *ld_window, *ld_title; */
-/*   GtkWidget *window; */
-/*   REQUIRE_ARGC("GTK-WINDOW-SET-TITLE", 2); */
-/*   ld_window = eval(FIRST_ARG, env); */
-/*   ld_title = eval(SECOND_ARG, env); */
-/*   window = get_special(ld_window, sym_limogtk_gpointer); */
-/*   REQUIRE_TYPE("GTK-WINDOW-SET-TITLE", ld_title, limo_TYPE_STRING); */
-/*   gtk_window_set_title(GTK_WINDOW(window), ld_title->data.d_string); */
-/*   return nil; */
-/* } */
-
-/* BUILTIN(builtin_gtk_window_set_default_size) */
-/* { */
-/*   limo_data *ld_window, *ld_width, *ld_height; */
-/*   GtkWidget *window; */
-/*   REQUIRE_ARGC("GTK-WINDOW-SET-DEFAULT-SIZE", 3); */
-/*   ld_window = eval(FIRST_ARG, env); */
-/*   ld_width = eval(SECOND_ARG, env); */
-/*   ld_height = eval(THIRD_ARG, env); */
-/*   window = get_special(ld_window, sym_limogtk_gpointer); */
-/*   REQUIRE_TYPE("GTK-WINDOW-SET-DEFAULT-SIZE", ld_width, limo_TYPE_GMPQ); */
-/*   REQUIRE_TYPE("GTK-WINDOW-SET-DEFAULT-SIZE", ld_height, limo_TYPE_GMPQ); */
-/*   gtk_window_set_default_size(GTK_WINDOW(window), GETINTFROMMPQ(ld_width), GETINTFROMMPQ(ld_height)); */
-/*   return nil; */
-/* } */
-
-/* BUILTIN(builtin_gtk_widget_show_all) */
-/* { */
-/*   limo_data *ld_widget; */
-/*   GtkWidget *widget; */
-/*   REQUIRE_ARGC("GTK-WIDGET-SHOW-ALL", 1); */
-/*   ld_widget = eval(FIRST_ARG, env); */
-/*   widget = get_special(ld_widget, sym_limogtk_gpointer); */
-/*   gtk_widget_show_all(GTK_WIDGET(widget)); */
-/*   return nil; */
-/* } */
-
 BUILTIN(builtin_g_application_run)
 {
   limo_data *ld_app;
@@ -88,7 +38,7 @@ static void *limogtk_signal_handler(GtkWidget *widget, gpointer *user_data)
   return result;
 }
 
-BUILTIN(builtin_g_signal_connect)
+BUILTIN(builtin_g_signal_connect_simple)
 {
   limo_data *ld_instance, *ld_signal, *ld_handler;
 
@@ -96,40 +46,43 @@ BUILTIN(builtin_g_signal_connect)
   ld_instance = eval(FIRST_ARG, env);
   ld_signal   = eval(SECOND_ARG, env);
   ld_handler  = eval(THIRD_ARG, env);
-  REQUIRE_TYPE("G-SIGNAL-CONNECT", ld_signal, limo_TYPE_STRING);
-  REQUIRE_TYPE("G-SIGNAL-CONNECT", ld_handler, limo_TYPE_LAMBDA);
-  REQUIRE_TYPE("G-SIGNAL-CONNECT", ld_instance, limo_TYPE_SPECIAL);
+  REQUIRE_TYPE("G-SIGNAL-CONNECT-SIMPLE", ld_signal, limo_TYPE_STRING);
+  REQUIRE_TYPE("G-SIGNAL-CONNECT-SIMPLE", ld_handler, limo_TYPE_LAMBDA);
+  REQUIRE_TYPE("G-SIGNAL-CONNECT-SIMPLE", ld_instance, limo_TYPE_SPECIAL);
   return make_number_from_long_long(g_signal_connect(G_OBJECT(CDR(ld_instance->data.d_special) -> data.d_special_intern),
                                                      ld_signal->data.d_string,
                                                      G_CALLBACK(limogtk_signal_handler),
                                                      (gpointer)make_cons(ld_handler, env)));
 }
 
-/* BUILTIN(builtin_gtk_application_new) */
-/* { */
-/*   limo_data *ld_app_id, *ld_app_flags, *ld_result; */
-/*   GtkApplication *app; */
-/*   char *app_id; */
-/*   GApplicationFlags flags; */
+BUILTIN(builtin_make_gtk_text_iter)
+{
+  gpointer iter;
+  iter = GC_malloc_atomic(sizeof (GtkTextIter));
+  return make_special(sym_limogtk_gpointer, iter);
+}
 
-/*   REQUIRE_ARGC("GTK-APPLICATION-NEW", 2); */
-/*   ld_app_id = eval(FIRST_ARG, env); */
-/*   ld_app_flags = eval(SECOND_ARG, env); */
-/*   if (is_nil(ld_app_id)) */
-/*     app_id = NULL; */
-/*   else { */
-/*     REQUIRE_TYPE("GTK-APPLICATION-NEW", ld_app_id, limo_TYPE_STRING); */
-/*     app_id = ld_app_id->data.d_string; */
-/*   } */
-/*   REQUIRE_TYPE("GTK-APPLICATION-NEW", ld_app_flags, limo_TYPE_GMPQ); */
-/*   flags  = GETINTFROMMPQ(ld_app_flags); */
-/*   app = gtk_application_new(app_id, flags); */
-/*   ld_result =  make_special(sym_limogtk_gpointer, app); */
-/*   GC_register_finalizer(ld_result, (GC_finalization_proc)limogtk_finalizer_g_object_unref, app, NULL, NULL); */
-/*   return ld_result; */
-/* } */
+BUILTIN(builtin_make_errorpp)
+{
+  GError **gerrpp;
+  gerrpp = GC_malloc_atomic(sizeof (GError *));
+  *gerrpp = NULL;
+  return make_special(sym_limogtk_gpointer, gerrpp);
+}
+
+BUILTIN(builtin_get_error)
+{
+  limo_data *ld_gerrpp;
+  ld_gerrpp = eval(FIRST_ARG, env);
+  GError **gerrpp = get_special(ld_gerrpp, sym_limogtk_gpointer);
+  if (*gerrpp == NULL)
+    return nil;
+  else
+    return make_string((*gerrpp)->message);
+}
 
 #include "_gtk-functions.h"
+#include "gtk-marshall.h"
 
 void limo_init_limogtk(limo_data *env)
 {
@@ -140,13 +93,12 @@ void limo_init_limogtk(limo_data *env)
   INS_LIMOGTK_BUILTIN(builtin_gtk_init, "GTK-INIT");
   INS_LIMOGTK_BUILTIN(builtin_gtk_init_check, "GTK-INIT-CHECK");
   INS_LIMOGTK_BUILTIN(builtin_g_application_run, "G-APPLICATION-RUN");
+  INS_LIMOGTK_BUILTIN(builtin_g_signal_connect_simple, "G-SIGNAL-CONNECT-SIMPLE");
   INS_LIMOGTK_BUILTIN(builtin_g_signal_connect, "G-SIGNAL-CONNECT");
-  /* INS_LIMOGTK_BUILTIN(builtin_gtk_application_new, "GTK-APPLICATION-NEW"); */
-  /* INS_LIMOGTK_BUILTIN(builtin_gtk_application_window_new, "GTK-APPLICATION-WINDOW-NEW"); */
-  /* INS_LIMOGTK_BUILTIN(builtin_gtk_widget_show_all, "GTK-WIDGET-SHOW-ALL"); */
-  /* INS_LIMOGTK_BUILTIN(builtin_gtk_window_set_default_size, "GTK-WINDOW-SET-DEFAULT-SIZE"); */
-  /* INS_LIMOGTK_BUILTIN(builtin_gtk_window_set_title, "GTK-WINDOW-SET-TITLE"); */
-
+  INS_LIMOGTK_BUILTIN(builtin_make_gtk_text_iter, "MAKE-GTK-TEXT-ITER");
+  INS_LIMOGTK_BUILTIN(builtin_make_errorpp, "MAKE-ERRORPP");
+  INS_LIMOGTK_BUILTIN(builtin_get_error, "GET-ERROR");
+  
   #include "_gtk-inserts.h"
   #include "_limogtk-enums.h"
 
