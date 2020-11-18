45bd70f40489e2bd5790d370464be632137958e2
mokrates <mokrates@users.sourceforge.net>
2020-11-18 02:01:44 +0100

    added string-utf8-length

--- libs/string_builtins/string_builtins.c
+++ libs/string_builtins/string_builtins.c
@@ -78,6 +78,27 @@ BUILTIN(builtin_chr)
   return str;
 }
 
+
+// stolen from https://stackoverflow.com/questions/32936646/getting-the-string-length-on-utf-8-in-c
+BUILTIN(builtin_string_utf8_length) {
+  limo_data *str;
+  char *s;
+  size_t count = 0;
+
+  if (list_length(arglist) != 2)
+    limo_error("(string-length STRING)");
+
+  str = eval(FIRST_ARG, env);
+  if (str->type != limo_TYPE_STRING)
+    limo_error("(string-length STRING)");
+
+  s = str->data.d_string;
+  while (*s) {
+    count += (*s++ & 0xC0) != 0x80;
+  }
+  return make_number_from_long_long(count);
+}
+
 void limo_init_string_builtins(limo_data *env)
 {
   INSBUILTIN(builtin_string_nth, "STRING-NTH");
@@ -85,4 +106,5 @@ void limo_init_string_builtins(limo_data *env)
   INSBUILTIN(builtin_stringp, "STRINGP");
   INSBUILTIN(builtin_ord, "ORD");
   INSBUILTIN(builtin_chr, "CHR");
+  INSBUILTIN(builtin_string_utf8_length, "STRING-UTF8-LENGTH");
 }
