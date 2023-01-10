63b4b9c1d69866882154192436e720e037a26360
mokrates <mokrates@users.sourceforge.net>
2023-01-10 20:17:14 +0100

    repaired limogtk build

--- libs/limogtk/generate_stuff.sh
+++ libs/limogtk/generate_stuff.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
+set -x
 echo '#include <gtk/gtk.h>' > _gtk-h.c
-i686-w64-mingw32-gcc $(i686-w64-mingw32-pkg-config gtk+-win32-3.0 --cflags) -E _gtk-h.c -o _gtk-h.E.c
+#i686-w64-mingw32-gcc $(i686-w64-mingw32-pkg-config gtk+-win32-3.0 --cflags) -E _gtk-h.c -o _gtk-h.E.c
+gcc $(pkg-config gtk+-3.0 --cflags) -E _gtk-h.c -o _gtk-h.E.c
 
 cat _gtk-h.E.c | grep -v '^#' | limo enums.limo | tee _limogtk-enums.h
 cat _gtk-h.E.c | grep -v '^#' | limo gen_builtins.limo
