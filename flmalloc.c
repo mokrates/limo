44abc70a1ad2a631a7b6f7b30631a1f528c3d9f6
Moritz Molle <mmolle@users.sourceforge.net>
2023-03-21 23:37:54 +0100

    flmalloc added

---
+++ flmalloc.c
@@ -0,0 +1,46 @@
+#include "limo.h"
+
+#include <stdint.h>
+
+// C99 or later
+#if UINTPTR_MAX == 0xFFFF
+  #define WORD_SHIFT 1
+#elif UINTPTR_MAX == 0xFFFFFFFF
+  #define WORD_SHIFT 2
+#elif UINTPTR_MAX == 0xFFFFFFFFFFFFFFFFu
+  #define WORD_SHIFT 3
+#else
+  #error TBD pointer size
+#endif
+
+void *flmalloc(size_t sz)
+{
+  void *result;
+  void **fl = pk_flmalloc_get();
+
+  if (!sz) return NULL;
+
+  int idx = (sz-1) << WORD_SHIFT;   // 1 WORD on idx=0
+
+  if (idx >= MAX_FLMALLOC_LISTS)
+    return GC_malloc(sz);    // we don't do objects that large
+  
+  if (!fl[idx])
+    fl[idx] = GC_malloc_many((idx+1)>>WORD_SHIFT);
+
+  result = fl[idx];
+  fl[idx] = *(void **)fl[idx];
+  return result;
+}
+
+void flfree(void *o, size_t sz)
+{
+  size_t idx;
+  void **fl = pk_flmalloc_get();  
+  if (sz < (1>>WORD_SHIFT)) return;   // we don't want this small objects
+  if (sz > (MAX_FLMALLOC_LISTS-1) >> WORD_SHIFT) return;  // .. and this is too large for us
+
+  idx = (sz-1)<<WORD_SHIFT;
+  *((void **)o) = fl[idx];
+  fl[idx] = o;
+}
