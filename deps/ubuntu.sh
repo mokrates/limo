611bbf050abcbacc303112aa535ca7243a9d9b25
mokrates <mokrates@users.sourceforge.net>
2024-03-09 17:31:40 +0100

    fixed ubuntu deps

--- deps/ubuntu.sh
+++ deps/ubuntu.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt install libgc-dev libgmp-dev libreadline-dev libssl-dev texinfo libsdl-dev libsdl-ttf2.0-dev libmhash-dev
+sudo apt install libgc-dev libgmp-dev libreadline-dev libssl-dev texinfo libsdl1.2-dev libsdl-ttf2.0-dev libmhash-dev
